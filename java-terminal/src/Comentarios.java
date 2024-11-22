2a4904db938a0bb0a41862f41efea052056b5165
Daniely <danixavier815@gmail.com>
2024-11-21 21:57:30 -0300

    java doc

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String [] args){
+        //Olá, eu sou um comentário em uma única linha
+
+        /* Olá
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+
+    // comando para criar o javadoc:
+    // javadoc -encoding UTF-8 -d ocencoding ISO-8859-1 -d ../docs src/*.java 
+
+    
+    
+}
